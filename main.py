6b8f94f45c0a0212923c5a7e96c618760b303c58
SaveUncut <101981660+SaveUncut@users.noreply.github.com>
2022-03-27 09:28:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -326,7 +326,7 @@ async def callback_handlers(bot: Client, cb: CallbackQuery):
                 user = await bot.get_chat_member(chat_id=(int(Config.UPDATES_CHANNEL) if Config.UPDATES_CHANNEL.startswith("-100") else Config.UPDATES_CHANNEL), user_id=cb.message.chat.id)
                 if user.status == "kicked":
                     await cb.message.edit(
-                        text="Sorry Sir, You are Banned to use me. Contact my [Support Group](https://t.me/linux_repo).",
+                        text="Sorry Sir, You are Banned to use me. Contact my [Support Team](https://t.me/linux_repo).",
                         parse_mode="markdown",
                         disable_web_page_preview=True
                     )
@@ -362,7 +362,7 @@ async def callback_handlers(bot: Client, cb: CallbackQuery):
         await cb.message.edit(
             text=Config.START_TEXT,
             parse_mode="Markdown",
-            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer - @AbirHasan2005", url="https://t.me/AbirHasan2005"), InlineKeyboardButton("Support Group", url="https://t.me/linux_repo")], [InlineKeyboardButton("Bots Channel", url="https://t.me/Discovery_Updates")]]),
+            reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Developer - @YourX", url="https://t.me/YourX")]]),
             disable_web_page_preview=True
         )
     elif "showThumbnail" in cb.data:
